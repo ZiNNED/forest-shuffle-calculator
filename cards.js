e55ef8d58ea38c2386a78ad3981d92ab5d8089d0
Dennis Smit <ZiNNED@users.noreply.github.com>
2026-08-24 13:04:02 +0000

    v1.11 - add Phoebus Apollo butterfly (base, tops, butterfly lookup scoring)

--- cards.js
+++ cards.js
@@ -1724,6 +1724,44 @@ const CARDS = [
   ],
   "tags": [],
   "attachedCards": {}
+},
+{
+  "id": "phoebusApollo",
+  "names": {
+    "en": "Phoebus Apollo",
+    "nl": "Kleine apollovlinder"
+  },
+  "zone": "tops",
+  "category": "butterfly",
+  "symbols": [
+    "butterfly",
+    "insect",
+    "alpine"
+  ],
+  "expansion": "base",
+  "scoring": [
+    {
+      "count": {
+        "of": "distinct",
+        "value": "butterfly"
+      },
+      "reward": {
+        "mode": "lookup",
+        "table": [
+          0,
+          3,
+          6,
+          12,
+          20,
+          35,
+          55
+        ],
+        "repeated": true
+      }
+    }
+  ],
+  "tags": [],
+  "attachedCards": {}
 }
-]
+];
 
